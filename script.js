6d521f4e60487bdf823654b588c6c103a9ee6d69
Mason Johnson <22926064+mason1566@users.noreply.github.com>
2024-03-13 21:01:24 -0700

    added basic profile card

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-/* USER CREATION */
-const userDataArray = []; // main user array. Each element in the array is to hold a user object.
+/* DOM MANIPULATION */
+const hideElement = (element) => element.style.display = "none";
+const showElement = (element) => element.style.display = "block";
 
-/* function to parse comma-separated user-data into objects. Places user objects in userDataArray */
+/* login view */
+const loginView = document.getElementById("loginView");
+const loginForm = document.getElementById("loginForm");
+const unameTextInput = document.getElementById("usernameInput");
+
+// overrides default form submit behaviour
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // done to prevent the page from refreshing (the default submit action)
+    let isValid = checkValidUser(unameTextInput.value);
+    if (isValid) {
+        hideElement(loginView);
+    }
+    else console.log("Invalid user");
+    console.log(unameTextInput.value)
+});
+
+
+
+/* profile view */
+const profileView = document.getElementById("profileView");
+
+const buildProfile = (userObj) => {
+    
+}
+
+
+
+/* users view */
+const usersView = document.getElementById("usersView");
+
+
+
+
+/* USER CREATION && HANDLING */
+/* function to parse comma-separated user-data into objects. Pushes user objects to userDataArray */
 const parseUserData = (users) => {
     let userLines = users.split("\n");
     
@@ -10,12 +45,11 @@ const parseUserData = (users) => {
         splitUserData.push(line.split(","));
     }
 
-    // 
+    // Creates an object for each array of user-data
     for (const userInfo of splitUserData) {
         let userObj = createUserObject(userInfo);
         userDataArray.push(userObj);
     }
-
 }
 
 const createUserObject = (userArray) => {
@@ -37,18 +71,15 @@ const printUsers = () => {
     }
 }
 
-
-
-/* DOM MANIPULATION */
-const form = document.getElementById("loginForm");
-
-// overrides default form submit behaviour
-form.addEventListener('submit', (e) => {
-    e.preventDefault(); // done to prevent the page from refreshing (the default submit action)
-    console.log("SUBMIT");
-});
+const checkValidUser = (username) => {
+    for (const user of userDataArray) {
+        if (user.username == username) return true;
+    }
+    return false;
+}
 
 
 /* MAIN LOGIC */
-parseUserData(userDataString);
+const userDataArray = []; // main user array. Each element in the array is to hold a user object.
+parseUserData(userDataString); // populate userDataArray with pre-made user-data
 
