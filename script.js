53f337da21a122b4f3c02e46c0a33f900446b889
Mason Johnson <22926064+mason1566@users.noreply.github.com>
2024-03-13 22:09:03 -0700

    added profile account info

--- script.js
+++ script.js
@@ -2,17 +2,29 @@
 const hideElement = (element) => element.style.display = "none";
 const showElement = (element) => element.style.display = "block";
 
+/* header */
+const navLoginButton = document.getElementById("navLoginButton");
+const navLogoutButton = document.getElementById("navLogoutButton");
+
 /* login view */
 const loginView = document.getElementById("loginView");
 const loginForm = document.getElementById("loginForm");
 const unameTextInput = document.getElementById("usernameInput");
+hideElement(navLogoutButton);
 
-// overrides default form submit behaviour
+// overrides default form submit behaviour. If username is correct, handles login logic
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault(); // done to prevent the page from refreshing (the default submit action)
-    let isValid = checkValidUser(unameTextInput.value);
+    let userInput = unameTextInput.value;
+
+    let isValid = checkValidUser(userInput); // checks user input against valid usernames, returning true if found.
     if (isValid) {
         hideElement(loginView);
+        hideElement(navLoginButton);
+
+        setupProfile(getUser(unameTextInput.value))
+        showElement(profileView);
+        showElement(navLogoutButton);
     }
     else console.log("Invalid user");
     console.log(unameTextInput.value)
@@ -23,15 +35,107 @@ loginForm.addEventListener('submit', (e) => {
 /* profile view */
 const profileView = document.getElementById("profileView");
 
-const buildProfile = (userObj) => {
-    
+let currentUser;
+
+const setupProfile = (userObj) => {
+    currentUser = userObj;
+
+    let profile;
+    if (currentUser.role == "user") profile = buildProfile(userObj);
+    else profile = buildAdminProfile(currentUser);
+
+    profileView.innerHTML = profile;
+
+    // adding event listeners for card links
+    document.getElementById("accountInfoLink").addEventListener('click', logout);
+    document.getElementById("viewUsersLink").addEventListener('click', logout);
+    document.getElementById("logoutLink").addEventListener('click', logout);
 }
 
+const buildProfile = (userObj) => {
+    let profileCard = `<div class="card card-style col-10 col-md-6 mx-auto" style="height: 470px;">
+                            <!-- profile body -->
+                            <div class="card-body p-0 pt-5 d-flex justify-content-start align-items-center flex-column">
+                                <div class="mx-auto"><img src="${userObj.image}.jpeg" class="profile-image" alt=""></div>
+                                <p class="card-title fw-bold fs-2 mt-3">Welcome, ${userObj.first_name}!</p>
+
+                                <div class="d-flex justify-content-between w-100 mt-3">
+                                    <div class="flex-grow-1 flex-fill text-center d-flex flex-column">
+                                        <p class="fs-5">How are you today?</p>
+                                        <div class="d-flex justify-content-center gap-4 mt-4 mb-3">
+                                            <button class="btn button-outline btn-danger fw-bold px-2 py-1">NOT GOOD</button>
+                                            <button class="btn button-outline btn-success fw-bold px-2 py-1">GOOD</button>
+                                        </div>
+                                    </div>
+                                    <div class="flex-grow-1 flex-fill">
+                                        <p class="fs-5 ps-5">Navigation:</p>
+                                        <ul class="ps-5 ms-5 list-unstyled">
+                                            <li><a href="#" id="accountInfoLink">Account Info</a></li>
+                                            <li><a href="#" id="viewUsersLink">View Users</a></li>
+                                            <li><a href="#" id="logoutLink">Logout</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`;
+    return profileCard;
+}
+
+const buildAdminProfile = (userObj) => {
+    let profileCard = `<div class="card card-style col-10 col-md-6 mx-auto" style="height: 470px;">
+                            <!-- profile body -->
+                            <div class="card-body p-0 pt-5 d-flex justify-content-start align-items-center flex-column">
+                                <div class="mx-auto"><img src="${userObj.image}.jpeg" class="profile-image" alt=""></div>
+                                <p class="card-title fw-bold fs-2 mt-3">Welcome, ${userObj.first_name}!</p>
+
+                                <div class="d-flex justify-content-between w-100 mt-3">
+                                    <div class="flex-grow-1 flex-fill text-center d-flex flex-column">
+                                        <p class="fs-5">How are you today?</p>
+                                        <div class="d-flex justify-content-center gap-4 mt-4 mb-3">
+                                            <button class="btn button-outline btn-danger fw-bold px-2 py-1">NOT GOOD</button>
+                                            <button class="btn button-outline btn-success fw-bold px-2 py-1">GOOD</button>
+                                        </div>
+                                    </div>
+                                    <div class="flex-grow-1 flex-fill">
+                                        <p class="fs-5 ps-5">Navigation:</p>
+                                        <ul class="ps-5 ms-5 list-unstyled">
+                                            <li><a href="#" id="accountInfoLink">Account Info</a></li>
+                                            <li><a href="#" id="viewUsersLink">View Users</a></li>
+                                            <li><a href="#" id="logoutLink">Logout</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`;
+    return profileCard;
+}
+
+const logout = () => {
+    currentUser = "";
+    profileView.innerHTML = ``;
+    hideAllViews();
+    hideElement(navLogoutButton);
+
+    showElement(navLoginButton);
+    showElement(loginView);
+}
+
+navLogoutButton.addEventListener('click', logout)
 
 
 /* users view */
 const usersView = document.getElementById("usersView");
 
+const displayAccountInfo = (userObj) => {
+
+}
+
+
+const hideAllViews = () => {
+    hideElement(loginView);
+    hideElement(profileView);
+    hideElement(usersView);
+}
 
 
 
@@ -78,8 +182,14 @@ const checkValidUser = (username) => {
     return false;
 }
 
+const getUser = (username) => {
+    for (const user of userDataArray) {
+        if (user.username == username) return user;
+    }
+    return null;
+}
+
 
 /* MAIN LOGIC */
 const userDataArray = []; // main user array. Each element in the array is to hold a user object.
 parseUserData(userDataString); // populate userDataArray with pre-made user-data
-
